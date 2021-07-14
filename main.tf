aae646896f5d6a0055499aabdff51407dd71f90b
AnhTran1610 <theanh161093@gmail.com>
2021-07-14 18:19:45 +0700

    .

--- main.tf
+++ main.tf
@@ -135,7 +135,6 @@ resource "aws_eip" "one" {
   vpc                       = true
   network_interface         = aws_network_interface.web-server-nic.id
   associate_with_private_ip = "10.0.1.50"
-  depends_on = aws_internet_gateway.gw
 }
 
 # 9. Create linux server and install/enable apache2
